92616e10f79c3bd334a63be58b1827864fa1adb6
nurzafri <nurzafri@gmail.com>
2022-10-31 14:36:06 +0800

    commit changes

---
+++ index.js
@@ -0,0 +1,11 @@
+//Summing of 2 + 3//
+//console.log(2 + 3);
+
+//console.log(undefined);
+//console.log(null);
+
+//array
+console.log(["a", "b", "c"])
+
+//object
+console.log({courseName : "JavaScript"})
